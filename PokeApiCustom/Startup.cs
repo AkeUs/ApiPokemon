ef2b86373871603a5e2361405573d95407be3a21
Miguel Anegel Ake Us <akeus_102292@hotmail.com>
2019-11-25 11:26:23 -0500

    Se actualiza version de Net Core 2.2 => 3.0

--- PokeApiCustom/Startup.cs
+++ PokeApiCustom/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PokeApiCustom.Repositories;
 using PokeApiCustom.Services;
+using Microsoft.Extensions.Hosting;
 
 namespace PokeApiCustom {
     
@@ -34,11 +35,12 @@ namespace PokeApiCustom {
                 //options.InstanceName = "PokeApi:";
             });
             
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options => options.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
