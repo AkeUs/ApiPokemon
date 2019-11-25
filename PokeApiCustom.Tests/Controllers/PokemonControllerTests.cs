519e958d4c944eb7e5976b1a0cfb5d81662faa4f
Miguel Anegel Ake Us <akeus_102292@hotmail.com>
2019-11-25 10:11:23 -0500

    Se agrega nuget moq para crear mocks de dependencias

---
+++ PokeApiCustom.Tests/Controllers/PokemonControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using PokeApiCustom.Controllers;
+using PokeApiCustom.Models;
+using PokeApiCustom.Repositories;
+using PokeApiCustom.Tests.Fakes;
+using Xunit;
+
+namespace PokeApiCustom.Tests.Controllers {
+    
+    public class PokemonControllerTests {
+
+        [Fact]
+        public void Get_ListPokemon_ReturnList() {
+            var pokemonRepositoryMock = new Mock<IPokemonRepository>();
+            pokemonRepositoryMock.Setup(x => x.GetPokemonList()).Returns(default(Task<List<Pokemon>>));
+            
+            var pokemonController = new PokemonController(pokemonRepositoryMock.Object);
+            var response = pokemonController.Get();
+
+            Assert.Null(response.Result);
+
+        }
+        
+    }
+}
